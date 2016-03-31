92268b357fc39a3773d833d12400c57909bcfa52
leowang721 <leowang721@gmail.com>
2016-03-31 20:00:59 +0800

    bump 0.0.3 use mini-event

---
+++ src/main.js
@@ -0,0 +1,22 @@
+/**
+ * @file fc-core
+ *
+ * @author Leo Wang(leowang721@gmail.com)
+ */
+
+import Promise from 'promise';
+import EventTarget from 'mini-events/EventTarget';
+
+import LifeStage from './LifeStage';
+import eventBus from './eventBus';
+import setImmediate from './setImmediate';
+import util from './util';
+
+export {
+    Promise,
+    LifeStage,
+    eventBus,
+    EventTarget,
+    setImmediate,
+    util
+};
